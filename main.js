9649d8e444a06a3461ab82294fbd61bac53956fe
Shabana-Fakhar <shabanafakhar0861@gmail.com>
2024-10-05 11:16:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,131 @@
+//Arithmatic operators +,-,*,/,%,++,--
+//var sum=10+20;
+//console.log(sum)
+//var sum=10-5;
+//console.log(sum)
+//var sum=10*10;
+//console.log(sum)
+//var sum=10%10;
+//console.log(sum)
+//var sum=20;
+//console.log(sum++)
+//console.log(sum)
+//var sum=20;
+//console.log(sum--)
+//console.log(sum)
+
+
+//comparison operator ==,===,!=,<,>,<=,>=
+//var sum=20==20;
+//console.log(sum)
+//var sum=20==="20";
+//console.log(sum)
+//var sum=20!=20;
+//console.log(sum)
+//var sum=20<=20;
+//console.log(sum)
+//var sum=20>=10;
+//console.log(sum)
+//logical operator &&,||,!
+//var sum=20==20 && 10==10;
+//console.log(sum)
+//var sum=20==20 || 10==10;
+//console.log(sum)
+//var sum=20==20;
+//console.log(!sum)
+//Assignment operator =,+=,-=,*=,/=
+//var sum=20;
+//var b=10;
+//sum/=b
+//console.log(sum)
+//console.log(b)
+//var sum=20;
+//var b=10;
+//var result1=sum=sum+b;
+//var result2=sum+=b;
+//console.log(result1)
+//console.log(result2)
+//Ternary operators if-else
+//var age=19;
+//console.log(isAdult)
+//conditional statements
+//var age=12
+//if(age>=22){
+    //console.log("yes is adult")
+
+//}else if(age>=21){
+   // console.log("you will be,next year")
+
+//}else if(age<=13){
+    //console.log("you are a teenagger")
+
+//} else{
+   // console.log("No,This ia not")
+
+//}
+//Switch statement
+//var age=22
+//witch(age){
+   // case 22:
+   // console.log("you are adult")
+   // break;
+    //case 21:
+       // console.log("Try next year")
+       // break;
+       // case 13:
+           // console.log("you are teenager")
+           // break;
+           // default:
+                //console.log("NO!you are not")
+//}
+//datatypes
+//strings type data
+//var  data="Hello pakistan"
+//numeric data
+//var num=1;
+//bolean type data
+//var data="false"
+//float typedata
+//var data=1.5;
+//To check datatypes
+//Number
+//var data=11.01;
+//var type=typeof(data);
+//console.log(type)
+//string
+//var data="hello pakistan"
+//var type=typeof(data)
+//console.log(type)
+//bolean
+
+//var data=true
+//var type=typeof(data)
+//console.log(type)
+//Null
+
+
+//varr data=true
+//var type=typeof(data)
+//console.log(type)
+//undefined
+//var data;
+//var type=typeof(data)
+//console.log(type)
+//object type data
+//var data={name:"shabana",age:22};
+//var type=typeof(data);
+//console.log(type)
+//console.log(data)
+//Functions
+function sum(a,b){
+    return a+b;
+}
+var num1=1;
+var num2=10;
+    
+var result1= sum(num1,num2);
+var result2=sum(10,30);
+
+console.log(result1)
+console.log(result2)
+
